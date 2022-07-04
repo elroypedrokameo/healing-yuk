36f7a3df0f0f9d51a3db1e888b9c617c0bab0933
elroypedrokameo <infinitygamexray@gmail.com>
2022-07-04 18:59:33 +0700

    feat: navbar

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">Healing Yuk</span>
+        <div className="navItems">
+          <button className="navButton">
+            Register
+          </button>
+          <button className="navButton">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
