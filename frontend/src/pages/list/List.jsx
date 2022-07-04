6acdf5e854da5d3eef11762a00fca3edfba49554
elroypedrokameo <infinitygamexray@gmail.com>
2022-07-04 22:25:26 +0700

    feat: search bar

--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../../components/navbar/header/Header'
+import Navbar from '../../components/navbar/Navbar'
 
 const List = () => {
   return (
-    <div>List</div>
+    <>
+      <Navbar />
+      <Header type="list" />
+    </>
   )
 }
 
