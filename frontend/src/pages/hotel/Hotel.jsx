5d9966cd9f317d9c37c43da46959cec37e34731f
elroypedrokameo <infinitygamexray@gmail.com>
2022-07-09 11:54:12 +0700

    feat: search result hotel page

--- frontend/src/pages/hotel/Hotel.jsx
+++ frontend/src/pages/hotel/Hotel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import './hotel.css'
 
 const Hotel = () => {
   return (
--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -5,6 +5,7 @@ import { useLocation } from "react-router-dom"
 import { useState } from "react"
 import { format } from "date-fns"
 import { DateRange } from "react-date-range"
+import SearchItem from "../../components/searchItem/SearchItem"
 
 const List = () => {
 
@@ -74,8 +75,19 @@ const List = () => {
                 </div>
               </div>
             </div>
+            <button>Search</button>
           </div>
-          <div className="listResult"></div>
+          <div className="listResult">
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+          </div>
         </div>
       </div>
     </>
